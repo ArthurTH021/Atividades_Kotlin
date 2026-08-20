734ea2562d1a6be329f15228021197ed2a58068a
Arthur Thomazoni <142926642+ArthurTH021@users.noreply.github.com>
2026-08-19 22:37:28 -0300

    Add files via upload

--- Revisao1/src/repositorio/JPA.kt
+++ Revisao1/src/repositorio/JPA.kt
@@ -34,10 +34,28 @@ class JPA(
         println("Salvando...")
         try {
             conectar()//Abre a conexão com o banco
-            c!!.createStatement().executeQuery("" +
-                    "INSERT INTO caixa_da_agua " +
+            val sql = "INSERT INTO caixa_da_agua " +
                     "(marca, modelo, dimensao, cor, material, formato, preco) " +
-                    "VALUES (${a.marca}, ${a.modelo}, ${a.dimensao}, ${a.cor}, ${a.material}, ${a.formato}, ${a.preco})")
+                    "VALUES (?, ?, ?, ?, ?, ?, ?)"
+
+            //Preparar lista para Double Precision
+            val doublePrecision = c!!.createArrayOf("float8", a.dimensao.toTypedArray())
+            //O typedArray() converte um Array para um tipo de dado legível para o Postgres
+
+            val stmt = c!!.prepareStatement(sql)
+
+            //Preparar as Variáveis para o banco
+            stmt.setString(1, a.marca)
+            stmt.setString(2, a.modelo)
+            stmt.setArray(3, doublePrecision)
+            stmt.setString(4, a.cor.name)
+            stmt.setString(5, a.material.name)
+            stmt.setString(6, a.formato)
+            stmt.setString(7, a.preco.toString())
+
+            stmt.executeUpdate()
+            stmt.close()//Encerra o Placeholder
+
             c!!.close()//Encerra a conexão com o banco
         }catch (e:SQLException){
             println("Não salvou: ${e.printStackTrace()}")
