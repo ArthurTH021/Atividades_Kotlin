4fbf8009710838534a48b9891c19298b8cca73bc
Arthur Thomazoni <142926642+ArthurTH021@users.noreply.github.com>
2026-09-14 14:28:54 -0300

    Delete Revisao1 - Copia directory

--- Revisao1 - Copia/src/enums/Material.kt
+++
@@ -1,5 +0,0 @@
-package enums
-
-enum class Material {
-    POLIETILENO, FIBRA_DE_VIDRO, INOX
-}
--- Revisao1 - Copia/src/sistema/caixadeagua/ListarCaixa.kt
+++
@@ -1,8 +0,0 @@
-package sistema.caixadeagua
-
-import repositorio.CRUDCaixaDaAgua
-
-fun listarCaixa() {
-    val CRUDCaixaDaAgua = CRUDCaixaDaAgua()
-    CRUDCaixaDaAgua.listar()
-}
